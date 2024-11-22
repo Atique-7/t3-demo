d1b5db2f4ff3b26746181e7a6bf739716347989c
Omkar <omkarmishra22@gmail.com>
2024-11-22 13:16:44 +0530

    Adding Undefined Values

--- src/app/biller/jobCard/[jobCardId]/test/page.tsx
+++ src/app/biller/jobCard/[jobCardId]/test/page.tsx
@@ -50,7 +50,7 @@ export default function page() {
   }, []);
 
   const getData = async () => {
-    await fetch(`http://localhost:3001/tally/updateInvoices`, {
+    await fetch(`http://localhost:3000/tally/updateInvoices`, {
       // await fetch(`https://t3-next-dev.vercel.app/tally/updateInvoices`, {
       method: "POST",
       body: JSON.stringify({ data: { lastSync: "18-Nov-24 00:00:00" } }),

1e813f5d0a0300cfb777817ad495f73fc71bc93e
Atique Khan <atique.khan@zeza.tech>
2024-12-08 19:24:51 +0530

    added and fixed advisor change

--- src/app/admin/manage-jobcards/page.tsx
+++ src/app/admin/manage-jobcards/page.tsx
@@ -1,35 +1,97 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { getCookie } from "cookies-next";
-import { JobCardsDataTable } from "@/components/data-tables/job-cards-data-table";
+import { getCookie, setCookie } from "cookies-next";
+
 import DisplayCard from "@/components/DisplayCard";
 import PartsPageSkeleton from "@/components/skeletons/PartsPageSkeleton";
 import { CarFront, Wrench, ListChecks } from "lucide-react";
-import { jobCardColumns } from "@/lib/column-definitions";
-import { getAllJobCards } from "@/lib/appwrite";
+import { tempCarsColumns } from "@/lib/column-definitions";
+import {
+  getAllJobCards,
+  getAllTempCars,
+  getLastJobCardNumber,
+  validateJobCardNumber,
+} from "@/lib/appwrite";
+import { TempCarsDataTable } from "@/components/data-tables/temp-cars-data-table";
+import { TempCar } from "@/lib/definitions";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { purposeOfVisits } from "@/lib/helper";
 
 type Props = {};
 
 const manageJobCardsAdmin = ({}: Props) => {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [user, setUser] = useState(null);
   const [totalNumberOfCars, setTotalNumberOfCars] = useState(0);
   const [numberOfCarsInProgress, setNumberOfCarsInProgress] = useState(0);
   const [completedJobCars, setCompletedJobCars] = useState(0);
   const [currentJobCards, setCurrentJobCards] = useState([]);
+  const [tempCars, setTempCars] = useState<TempCar[]>([]);
+
+  const [servicePOV, setServicePOV] = useState<string[]>([]);
+
+  let currentCounter: number;
 
   useEffect(() => {
+    let povArr: string[] = [];
+
     const getUser = () => {
       const token = getCookie("user");
 
       const parsedToken = JSON.parse(String(token));
       console.log(parsedToken);
+
+      setUser(parsedToken);
+
+      let foundObjs;
+
+      if (parsedToken.labels[0] == "admin") {
+        foundObjs = purposeOfVisits;
+      } else {
+        foundObjs = purposeOfVisits.filter(
+          (pov) =>
+            pov.code == parsedToken.labels[2] ||
+            pov.code == parsedToken.labels[3]
+        );
+      }
+      foundObjs.map((pov) => povArr.push(pov.description));
+
+      setServicePOV(povArr);
+
       setName(parsedToken.name);
+      setEmail(parsedToken.email);
+      console.log(email);
+    };
+
+    const getTempCars = async () => {
+      const allTempCars = await getAllTempCars();
+      const token = getCookie("user");
+      const parsedToken = JSON.parse(String(token));
+      console.log("TEMP CARS - ", allTempCars);
+
+      if (parsedToken.labels[0] == "admin") {
+        setTempCars(allTempCars.documents);
+      }
     };
 
     const getJobCards = async () => {
       const allJobCards = await getAllJobCards();
       console.log("THESE ARE THE CURRENT JOB CARDS - ", allJobCards);
+      //currentCounter = allJobCards.documents[0].jobCardNumber + 1;
+
+      //Set jobcard counter
+      currentCounter = (await getLastJobCardNumber()) + 1;
+      currentCounter = await validateJobCardNumber(currentCounter);
+      setCookie("currentCounter", JSON.stringify(currentCounter));
       setTotalNumberOfCars(allJobCards.total);
       setCurrentJobCards(allJobCards.documents);
 
@@ -46,45 +108,14 @@ const manageJobCardsAdmin = ({}: Props) => {
       setCompletedJobCars(completedJobCarsArray.length);
     };
 
-    const processJobCards = async () => {
-      try {
-        const allJobCards = await getAllJobCards();
-        console.log("Fetched job cards:", allJobCards);
-
-        // Initialize a map to count cars for each advisor
-        const advisorWorkload: { [email: string]: number } = {};
-
-        allJobCards.documents.forEach((jobCard: any) => {
-          if (jobCard.purposeOfVisitAndAdvisors) {
-            // Parse the purposeOfVisitAndAdvisors field
-            const advisors = Array.isArray(jobCard.purposeOfVisitAndAdvisors)
-              ? jobCard.purposeOfVisitAndAdvisors
-              : JSON.parse(jobCard.purposeOfVisitAndAdvisors);
-
-            advisors.forEach((advisor: any) => {
-              if (advisor.advisorEmail) {
-                advisorWorkload[advisor.advisorEmail] =
-                  (advisorWorkload[advisor.advisorEmail] || 0) + 1;
-              }
-            });
-          }
-        });
-
-        console.log("Advisor workload summary:", advisorWorkload);
-        return advisorWorkload;
-      } catch (error) {
-        console.error("Error processing job cards:", error);
-        throw error;
-      }
-    };
-
     getUser();
     getJobCards();
+    getTempCars();
   }, []);
 
   return (
-    <div className="flex flex-col w-[90%] mt-10">
-      {!name && currentJobCards ? (
+    <div className="flex flex-col w-[90%] mt-32">
+      {!(name && tempCars) ? (
         <PartsPageSkeleton />
       ) : (
         <>
@@ -92,7 +123,7 @@ const manageJobCardsAdmin = ({}: Props) => {
             <div className="font-semibold text-3xl">Hello {name}! </div>
             <div className="font-medium">T3, Mira Road</div>
           </div>
-          <div className="flex flex-row space-x-8 mt-16 w-full justify-center">
+          <div className="hidden lg:flex flex-row space-x-8 mt-16 w-full justify-center lg:justify-normal">
             <DisplayCard
               icon={<CarFront />}
               desc={"Cars so far this month"}
@@ -109,11 +140,47 @@ const manageJobCardsAdmin = ({}: Props) => {
               value={completedJobCars}
             />
           </div>
+          <div className="flex lg:hidden w-full justify-center mt-10">
+            <Carousel className="w-[70%]">
+              <CarouselContent>
+                <CarouselItem>
+                  <div className="p-1">
+                    <DisplayCard
+                      icon={<CarFront />}
+                      desc={"Cars so far this month"}
+                      value={totalNumberOfCars}
+                    />
+                  </div>
+                </CarouselItem>
+                <CarouselItem>
+                  <div className="p-1">
+                    <DisplayCard
+                      icon={<Wrench />}
+                      desc={"In Progress"}
+                      value={numberOfCarsInProgress}
+                    />
+                  </div>
+                </CarouselItem>
+                <CarouselItem>
+                  <div className="p-1">
+                    <DisplayCard
+                      icon={<ListChecks />}
+                      desc={"Completed"}
+                      value={completedJobCars}
+                    />
+                  </div>
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
           <div className="flex flex-col mt-16">
-            <div className="font-semibold text-2xl mb-5">Cars</div>
-            <JobCardsDataTable
-              columns={jobCardColumns}
-              data={currentJobCards}
+            <div className="font-semibold text-2xl mb-5">Cars in Garage</div>
+            <TempCarsDataTable
+              columns={tempCarsColumns}
+              data={tempCars}
+              povCategories={servicePOV}
             />
           </div>
         </>
--- src/lib/column-definitions.tsx
+++ src/lib/column-definitions.tsx
@@ -431,69 +431,69 @@ export const tempCarsColumns: ColumnDef<TempCar>[] = [
       const parsedToken = JSON.parse(String(token));
       const userAccess = parsedToken.labels[0];
 
-      const advisorEmail = parsedToken.email;
       const purposeOfVisitAndAdvisors = convertStringsToArray(
         tempCar.purposeOfVisitAndAdvisors
       );
+
+      // For Case: Service;
+      const advisorEmail = parsedToken.email;
       const advisorInfo = purposeOfVisitAndAdvisors.find(
         (pov: any) => pov.advisorEmail === advisorEmail
       );
 
       let currentCounter = getCookie("currentCounter");
 
-      const [selectedAdvisor, setSelectedAdvisor] = useState(advisorEmail);
-      const [allAdvisors, setAllAdvisors] = useState<any[]>([]);
-
-      const [open, setOpen] = useState(false);
-
-      const handleCloseDialog = () => {
-        // Reset the selected advisor when dialog is closed
-        setSelectedAdvisor(advisorEmail);
-        setOpen(false);
-      };
+      const [selectedPovCode, setSelectedPovCode] = useState<number | null>(
+        null
+      );
+      const [selectedAdvisor, setSelectedAdvisor] = useState<string>("");
+      const [allAdvisors, setAllAdvisors] = useState<Record<number, any[]>>({});
 
       useEffect(() => {
         const fetchUsers = async () => {
           const users = await listAllUsers();
-          const advisorsMap = users
-            .filter((user: any) => {
-              const { advisorRoleId } = user.prefs;
-              let roleIds;
-              if (advisorRoleId) {
-                roleIds = JSON.parse(advisorRoleId);
-              }
-              for (let roleId in roleIds) {
-              }
-              return advisorRoleId === String(advisorInfo?.purposeOfVisitCode);
-            })
-            .map((user: any) => ({
-              email: user.email,
-              name: user.name,
-            }));
+          console.log("USERS", users);
+
+          // Map advisors for each purposeOfVisitCode
+          const advisorsMap = purposeOfVisitAndAdvisors.reduce(
+            (map: any, pov: any) => {
+              const matchingAdvisors = users.filter((user: any) => {
+                const roleIds = JSON.parse(user.prefs.advisorRoleId || "[]");
+                return roleIds.includes(pov.purposeOfVisitCode);
+              });
+              map[pov.purposeOfVisitCode] = matchingAdvisors.map(
+                (user: any) => ({
+                  email: user.email,
+                  name: user.name,
+                })
+              );
+              return map;
+            },
+            {}
+          );
+
           setAllAdvisors(advisorsMap);
         };
+
         fetchUsers();
-      }, [advisorInfo]);
+      }, []);
 
-      const handleSave = async () => {
-        // Find and update the specific advisor entry in the purposeOfVisitAndAdvisors array
+      const handleAdvisorChange = async () => {
+        if (!selectedPovCode) return;
+
+        // Update the specific entry in purposeOfVisitAndAdvisors
         const updatedPurposeOfVisitAndAdvisors = purposeOfVisitAndAdvisors.map(
-          (pov: any) => {
-            if (pov.advisorEmail === advisorInfo.advisorEmail) {
-              return { ...pov, advisorEmail: selectedAdvisor }; // Update the advisor's email
-            }
-            return pov; // Leave other items unchanged
-          }
+          (pov: any) =>
+            pov.purposeOfVisitCode === selectedPovCode
+              ? { ...pov, advisorEmail: selectedAdvisor }
+              : pov
         );
-        console.log(updatedPurposeOfVisitAndAdvisors);
-        console.log(tempCar.purposeOfVisitAndAdvisors);
 
-        // Call Appwrite's updateDocument method to update the tempCar with the new advisor email
         try {
           await databases.updateDocument(
             config.databaseId,
-            config.tempCarsCollectionId, // collectionId
-            tempCar.$id, // documentId
+            config.tempCarsCollectionId,
+            tempCar.$id,
             {
               purposeOfVisitAndAdvisors: convertToStrings(
                 updatedPurposeOfVisitAndAdvisors
@@ -501,12 +501,12 @@ export const tempCarsColumns: ColumnDef<TempCar>[] = [
             }
           );
           // Close the dialog after saving
-          setOpen(false);
           setTimeout(() => {
             window.location.reload(); // Refresh the page
-          }, 500);
+          }, 2000);
           toast("Advisor updated successfully! \u2705");
           console.log("Advisor updated successfully");
+          setSelectedPovCode(null);
         } catch (error) {
           console.error("Error updating advisor:", error);
         }
@@ -526,6 +526,141 @@ export const tempCarsColumns: ColumnDef<TempCar>[] = [
       };
 
       switch (userAccess) {
+        case "admin":
+          return (
+            <div>
+              {purposeOfVisitAndAdvisors.map((pov: any) => (
+                <div key={pov.purposeOfVisitCode} className="mb-4">
+                  <h3 className="text-lg font-semibold">{pov.description}</h3>
+                  {pov.open === false ? (
+                    <button
+                      onClick={() => {
+                        setSelectedPovCode(pov.purposeOfVisitCode);
+                        setSelectedAdvisor(pov.advisorEmail);
+                      }}
+                      className="text-blue-500 underline"
+                    >
+                      Change Advisor
+                    </button>
+                  ) : (
+                    <p className="text-red-500">
+                      Cannot change advisor for open Job Card.
+                    </p>
+                  )}
+                </div>
+              ))}
+
+              {selectedPovCode && (
+                <Dialog
+                  open={!!selectedPovCode}
+                  onOpenChange={() => setSelectedPovCode(null)}
+                >
+                  <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                      <DialogTitle>Change Advisor</DialogTitle>
+                      <DialogDescription>
+                        Select a new advisor for this purpose of visit.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                      <Select
+                        value={selectedAdvisor}
+                        onValueChange={(value) => setSelectedAdvisor(value)}
+                      >
+                        <SelectTrigger className="col-span-3">
+                          <SelectValue placeholder="Select an advisor" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {allAdvisors[selectedPovCode]?.map((advisor) => (
+                            <SelectItem
+                              key={advisor.email}
+                              value={advisor.email}
+                            >
+                              {advisor.name}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </div>
+                    <DialogFooter>
+                      <Button
+                        type="submit"
+                        className="bg-red-500"
+                        onClick={handleAdvisorChange}
+                      >
+                        Save
+                      </Button>
+                      <Button
+                        type="submit"
+                        className="bg-red-500"
+                        onClick={() => setSelectedPovCode(null)}
+                      >
+                        Cancel
+                      </Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+              )}
+            </div>
+          );
+        // return (
+        //   advisorInfo.open === false && (
+        //     <div className="p-2">
+        //       <Dialog open={open} onOpenChange={setOpen}>
+        //         <DialogTrigger asChild>
+        //           <Button
+        //             variant="outline"
+        //             className="border border-red-500 text-red-500"
+        //           >
+        //             Change Advisor
+        //           </Button>
+        //         </DialogTrigger>
+        //         <DialogContent className="sm:max-w-[425px]">
+        //           <DialogHeader>
+        //             <DialogTitle>Change Advisor</DialogTitle>
+        //             <DialogDescription>
+        //               Select a new advisor from the list below
+        //             </DialogDescription>
+        //           </DialogHeader>
+        //           <div className="grid gap-4 py-4">
+        //             <Select
+        //               value={selectedAdvisor}
+        //               onValueChange={(value) => setSelectedAdvisor(value)}
+        //             >
+        //               <SelectTrigger
+        //                 id="advisorSelect"
+        //                 className="col-span-3"
+        //               >
+        //                 <SelectValue placeholder="Select an advisor" />
+        //               </SelectTrigger>
+        //               <SelectContent>
+        //                 {allAdvisors.map((advisor) => (
+        //                   <SelectItem
+        //                     key={advisor.email}
+        //                     value={advisor.email}
+        //                   >
+        //                     {advisor.name}
+        //                   </SelectItem>
+        //                 ))}
+        //               </SelectContent>
+        //             </Select>
+        //           </div>
+        //           <DialogFooter>
+        //             <Button
+        //               className="bg-red-500 text-white"
+        //               onClick={handleSave}
+        //             >
+        //               Save
+        //             </Button>
+        //             <DialogClose asChild>
+        //               <Button onClick={handleCloseDialog}>Close</Button>
+        //             </DialogClose>
+        //           </DialogFooter>
+        //         </DialogContent>
+        //       </Dialog>
+        //     </div>
+        //   )
+        // );
         case "security":
           return (
             <div className="p-2">
@@ -551,102 +686,6 @@ export const tempCarsColumns: ColumnDef<TempCar>[] = [
               >
                 {advisorInfo.open === false ? "Create" : "View"}
               </Link>
-
-              {/* { (userAccess === "service" && advisorInfo.open === false) &&
-              <div className="p-2">
-              <Dialog open={isDialogOpen} onOpenChange={handleDialogClose}>
-                <DialogTrigger asChild>
-                  <Button variant="outline" className="border bordre-red-500 text-red-500">
-                    Change Advisor
-                  </Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px]">
-                  <DialogHeader>
-                    <DialogTitle>Change Advisor</DialogTitle>
-                    <DialogDescription>Select a new advisor from the list below</DialogDescription>
-                  </DialogHeader>
-                  <div className="grid gap-4 py-4">
-                    <Select
-                        value={selectedAdvisor}
-                        onValueChange={(value) => setSelectedAdvisor(value)}
-                      >
-                        <SelectTrigger id="advisorSelect" className="col-span-3">
-                          <SelectValue placeholder="Select an advisor" />
-                        </SelectTrigger>
-                        <SelectContent>
-                          {allAdvisors.map((advisor) => (
-                            <SelectItem key={advisor.email} value={advisor.email}>
-                              {advisor.name}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                  </div>
-                  <DialogFooter>
-                    <Button className="bg-red-500 text-white" onClick={handleSave}>
-                      Save
-                    </Button>
-                  </DialogFooter>
-                </DialogContent>
-              </Dialog>
-              </div>
-              } */}
-              {userAccess === "admin" && advisorInfo.open === false && (
-                <div className="p-2">
-                  <Dialog open={open} onOpenChange={setOpen}>
-                    <DialogTrigger asChild>
-                      <Button
-                        variant="outline"
-                        className="border border-red-500 text-red-500"
-                      >
-                        Change Advisor
-                      </Button>
-                    </DialogTrigger>
-                    <DialogContent className="sm:max-w-[425px]">
-                      <DialogHeader>
-                        <DialogTitle>Change Advisor</DialogTitle>
-                        <DialogDescription>
-                          Select a new advisor from the list below
-                        </DialogDescription>
-                      </DialogHeader>
-                      <div className="grid gap-4 py-4">
-                        <Select
-                          value={selectedAdvisor}
-                          onValueChange={(value) => setSelectedAdvisor(value)}
-                        >
-                          <SelectTrigger
-                            id="advisorSelect"
-                            className="col-span-3"
-                          >
-                            <SelectValue placeholder="Select an advisor" />
-                          </SelectTrigger>
-                          <SelectContent>
-                            {allAdvisors.map((advisor) => (
-                              <SelectItem
-                                key={advisor.email}
-                                value={advisor.email}
-                              >
-                                {advisor.name}
-                              </SelectItem>
-                            ))}
-                          </SelectContent>
-                        </Select>
-                      </div>
-                      <DialogFooter>
-                        <Button
-                          className="bg-red-500 text-white"
-                          onClick={handleSave}
-                        >
-                          Save
-                        </Button>
-                        <DialogClose asChild>
-                          <Button onClick={handleCloseDialog}>Close</Button>
-                        </DialogClose>
-                      </DialogFooter>
-                    </DialogContent>
-                  </Dialog>
-                </div>
-              )}
             </div>
           );
 

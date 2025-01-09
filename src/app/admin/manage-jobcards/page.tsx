14d935c0fbde0e421094a759959f900d64e2df92
omi265 <omkarmishra22@gmail.com>
2025-01-10 03:46:26 +0530

    Update getTempCars function to include additional temp car status in API call

--- src/app/admin/manage-jobcards/page.tsx
+++ src/app/admin/manage-jobcards/page.tsx
@@ -68,7 +68,7 @@ const manageJobCardsAdmin = ({}: Props) => {
     };
 
     const getTempCars = async () => {
-      const allTempCars = await getAllTempCars([0, 1]);
+      const allTempCars = await getAllTempCars([0, 1, 2]);
       const token = getCookie("user");
       const parsedToken = JSON.parse(String(token));
       console.log("TEMP CARS - ", allTempCars);

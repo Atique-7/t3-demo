4919e56bbab4127c1df54140ec18bcfa8f377caa
omi265 <omkarmishra22@gmail.com>
2025-01-29 06:16:11 +0530

    Update RevenueHistory component to change active chart button background color to red on active state

--- src/components/graphs/RevenueHistory.tsx
+++ src/components/graphs/RevenueHistory.tsx
@@ -193,7 +193,7 @@ export function RevenueHistory({ jobCards, currentSelectedTimeline }: any) {
               <button
                 key={chart}
                 data-active={activeChart === chart}
-                className="flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l data-[active=true]:bg-muted/50 sm:border-l sm:border-t-0 sm:px-8 sm:py-6"
+                className="flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l data-[active=true]:bg-red-100 sm:border-l sm:border-t-0 sm:px-8 sm:py-6"
                 onClick={() => setActiveChart(chart)}
               >
                 <span className="text-xs text-muted-foreground">
